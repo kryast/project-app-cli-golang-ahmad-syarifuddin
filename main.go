814409f15ff8155f595e26f2e0178ca1d5340399
Ahmad Syarifuddin <oden.psht@gmail.com>
2024-10-18 21:05:02 +0700

    CRUD v0.01

--- main.go
+++ main.go
@@ -1,21 +1,28 @@
 package main
 
 import (
-	"fmt"
-	"project-app-cli-golang-ahmad-syarifuddin/auth"
+	"project-app-cli-golang-ahmad-syarifuddin/data"
 )
 
 func main() {
-	auth.DaftarAkun()
-	auth.DaftarAkun()
+	// auth.DaftarAkun()
+	// auth.DaftarAkun()
 
-	fmt.Println(auth.DataAkun)
+	// fmt.Println(auth.DataAkun)
 
-	err := auth.Login()
-	if err != nil {
-		fmt.Println("Error:", err)
-	} else {
-		fmt.Printf("Berhasil Login")
-	}
+	// err := auth.Login()
+	// if err != nil {
+	// 	fmt.Println("Error:", err)
+	// } else {
+	// 	fmt.Printf("Berhasil Login")
+	// }
+
+	data.InputData()
+	data.InputData()
+	// data.TampilkanSemuaData()
+	data.UpdateData()
+	// data.TampilkanSemuaData()
+	data.DeleteData()
+	data.TampilkanSemuaData()
 
 }
