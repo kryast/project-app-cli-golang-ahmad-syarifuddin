e2430725c92f99749b088fe3493508b725265fe8
Ahmad Syarifuddin <oden.psht@gmail.com>
2024-10-18 21:14:59 +0700

    auth v0.04

--- auth/daftar.go
+++ auth/daftar.go
@@ -1,6 +1,9 @@
 package auth
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 type Akun struct {
 	username, password string
@@ -8,15 +11,35 @@ type Akun struct {
 
 var DataAkun []Akun
 
-func DaftarAkun() (Akun, []Akun) {
+func DaftarAkun() (Akun, []Akun, error) {
 	var username string
 	var password string
 
-	fmt.Print("Masukkan Username :")
+	fmt.Print("Masukkan Username: ")
 	fmt.Scan(&username)
-	fmt.Print("Masukkan Password :")
+	fmt.Print("Masukkan Password: ")
 	fmt.Scan(&password)
 
+	if err := validasiAkun(username, password); err != nil {
+		return Akun{}, DataAkun, err
+	}
+
 	DataAkun = append(DataAkun, Akun{username, password})
-	return Akun{username, password}, DataAkun
+	return Akun{username, password}, DataAkun, nil
+}
+
+func validasiAkun(username, password string) error {
+	for _, akun := range DataAkun {
+		if akun.username == username {
+			return errors.New("username sudah digunakan")
+		}
+	}
+	if len(username) < 3 {
+		return errors.New("username minimal 3 karakter")
+	}
+	if len(password) < 6 {
+		return errors.New("password minimal 6 karakter")
+	}
+
+	return nil
 }
--- main.go
+++ main.go
@@ -1,28 +1,28 @@
 package main
 
 import (
-	"project-app-cli-golang-ahmad-syarifuddin/data"
+	"fmt"
+	"project-app-cli-golang-ahmad-syarifuddin/auth"
 )
 
 func main() {
 	// auth.DaftarAkun()
 	// auth.DaftarAkun()
 
-	// fmt.Println(auth.DataAkun)
+	_, _, err := auth.DaftarAkun()
+	if err != nil {
+		fmt.Println("Error:", err)
+	} else {
+		fmt.Printf("Berhasil Daftar")
+	}
 
-	// err := auth.Login()
-	// if err != nil {
-	// 	fmt.Println("Error:", err)
-	// } else {
-	// 	fmt.Printf("Berhasil Login")
-	// }
-
-	data.InputData()
-	data.InputData()
+	fmt.Println(auth.DataAkun)
+	// data.InputData()
+	// data.InputData()
 	// data.TampilkanSemuaData()
-	data.UpdateData()
+	// data.UpdateData()
 	// data.TampilkanSemuaData()
-	data.DeleteData()
-	data.TampilkanSemuaData()
+	// data.DeleteData()
+	// data.TampilkanSemuaData()
 
 }
